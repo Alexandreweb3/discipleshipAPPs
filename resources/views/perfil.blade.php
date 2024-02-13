ad3d90b29d506699737b5f22f777a4c8aec312d1
Davi Diaz <felipediaz3974@gmail.com>
2024-02-13 11:40:34 -0300

    correção da rota do perfil

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=M, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Meu Perfil</title>
 </head>
 <body>
-    <h1>Deseja mudar seu perfil?</h1>
+    
 </body>
 </html>
