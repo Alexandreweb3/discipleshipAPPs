09a15c3f1040bc9304bcaa1503527be82ae15a8b
Davi Diaz <felipediaz3974@gmail.com>
2024-02-13 11:52:41 -0300

    create menu tela de perfil

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -5,8 +5,79 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Meu Perfil</title>
+    <style>
+       @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,500;0,700;1,300&display=swap');
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Poppins', sans-serif;
+    }
+
+    section {
+        position: relative;
+        width: 100%;
+        min-height: 100vh;
+        padding: 100px 20px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background: #121212;
+        overflow: hidden;
+    }
+
+    header {
+        position: absolute;
+        background-color: black;
+        top: 0;
+        left: 0;
+        width: 100%;
+        padding: 10px 20px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    header .logo {
+        position: relative;
+        max-width: 150px;
+    }
+
+    header .navegation ul {
+        position: relative;
+        display: flex;
+    }
+
+    header .navegation li {
+        list-style: none;
+    }
+
+    header .navegation li a {
+        display: inline-block;
+        color: #ffffff;
+        font-weight: 500;
+        text-decoration: none;
+        font-size: 16px;
+        margin-left: 20px;
+        transition: color 0.3s ease-in-out;
+    }
+
+    header .navegation li a:hover {
+        color: #0D4DC7;
+    }
+    </style>
 </head>
 <body>
-    
+    <header>
+        <a href={{ route('index') }}><img src="{{ 'storage/logoievv.png' }}" alt="." class="logo"></a>
+        <nav class="navegation">
+            <ul>
+                <li><a href="{{ route('index') }}">Home</a></li>
+                <li><a href="{{ route('contato') }}">Contatos</a></li>
+                <li><a href="{{ route('location') }}">Localização</a></li>
+                <li><a href="{{ route('index') }}">Sair</a></li>
+            </ul>
+        </nav>
+    </header>
 </body>
 </html>
