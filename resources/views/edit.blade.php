1893f1e3e636c5edb1f72bc5a1bbcbc839f4d76b
felipediaz3974@gmail.com <felipediaz3974@gmail.com>
2024-06-18 17:30:46 -0300

    icon spotify

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -4,8 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Discipulado</title>
+    <style>
+        
+    </style>
 </head>
 <body>
-    
+    <header>
+            <a href="{{ route('index') }}"><img src="\storage\logoievv.png" alt="" class="logo"></a>
+            <nav class="navegation">
+                <ul>
+                    <li><a href="{{ route('location') }}">Perfil</a></li>
+                    <li><a href="{{ route('index') }}">Sair</a></li>
+                </ul>
+            </nav>
+        </header>
 </body>
 </html>
