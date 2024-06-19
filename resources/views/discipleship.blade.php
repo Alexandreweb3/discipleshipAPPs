8b1ac8afac0ee11bacc8b26535aab4d35d151080
felipediaz3974@gmail.com <felipediaz3974@gmail.com>
2024-06-19 16:23:51 -0300

    created student support

--- resources/views/discipleship.blade.php
+++ resources/views/discipleship.blade.php
@@ -13,31 +13,35 @@
             font-family: 'Poppins', sans-serif;
         }
 
+        body {
+            background-color: #121212;
+            color: #FCDC13;
+            padding: 0;
+            margin: 0;
+        }
+
         section {
-            position: relative;
-            width: 100%;
-            min-height: 100vh;
-            padding: 100px;
             display: flex;
             justify-content: space-between;
-            align-items: center;
-            background: #121212;
-            overflow: hidden;
+            align-items: flex-start;
+            padding: 100px;
+            min-height: 100vh;
+            box-sizing: border-box;
         }
 
         header {
-            position: absolute;
-            top: 0;
-            left: 0;
             width: 100%;
             padding: 40px 100px;
             display: flex;
-            align-items: center;
             justify-content: space-between;
+            align-items: center;
+            background-color: #121212;
+            z-index: 1000;
+            top: 0;
+            left: 0;
         }
 
         header .logo {
-            position: relative;
             max-width: 150px;
         }
 
@@ -52,7 +56,7 @@
 
         header .navegation li a {
             display: inline-block;
-            color: #fff;
+            color: #FCDC13;
             font-weight: 500;
             text-decoration: none;
             font-size: 19px;
@@ -60,91 +64,152 @@
         }
 
         header .navegation li a:hover {
-            color: #FCDC13;
+            color: #fff;
         }
 
         nav.filter {
             position: absolute;
             top: 120px;
             right: 95px;
-        }
-
-        nav.filter-section {
-            padding: 4px;
-            border-radius: 4px;
+            z-index: 1000;
         }
 
         nav.filter select {
-            padding: 4px;
+            padding: 8px;
             background-color: #000000;
+            color: #FCDC13;
+            border-radius: 4px;
+            border: none;
+            cursor: pointer;
+        }
+
+        nav.filter select option:hover {
+            background-color: #333333;
+        }
+
+        .myclasses {
+            display: flex;
+            align-items: flex-start;
+            margin-top: 15px;
+            padding-top: 20px;
+            background-color: #121212;
+            color: #FCDC13;
+            width: 100%;
+            box-sizing: border-box;
+            justify-content: center;
+            flex-wrap: wrap;
+        }
+
+        .myclasses iframe {
+            width: 50%;
+            height: 350px;
+            border: none;
+            align-self: flex-start;
+        }
+
+        .myclasses .text-content {
+            flex: 1;
+            padding: 0 20px;
+            margin-top: 20px;
+            text-align: left;
+        }
+
+        .myclasses .text-content h1 {
+            font-size: 24px;
+            margin-bottom: 10px;
+        }
+
+        .myclasses .text-content p {
+            font-size: 16px;
+            line-height: 1.6;
+            margin-bottom: 20px;
+        }
+
+        .deliveringsummary {
+            text-align: left;
+            padding: 50px;
+            background-color: #121212;
+            margin-top: 20px;
+            color: #FCDC13;
+            width: 50%;
+        }
+
+        .deliveringsummary h1 {
+            font-size: 24px;
+            margin-bottom: 20px;
+        }
+
+        .deliveringsummary p {
+            font-size: 16px;
+            line-height: 1.6;
+            margin-bottom: 20px;
+        }
+
+        .form-container {
+            width: 50%;
+            background-color: #121212;
+            padding: 50px;
+            margin-top: 20px;
+            /* Espaçamento entre a seção anterior e esta */
+            color: #FCDC13;
+        }
+
+        .form-container h2 {
+            font-size: 24px;
+            margin-bottom: 20px;
+        }
+
+        .form-container form {
+            display: flex;
+            flex-direction: column;
+        }
+
+        .form-container label {
+            margin-bottom: 10px;
+        }
+
+        .form-container input,
+        .form-container textarea {
+            padding: 10px;
+            margin-bottom: 20px;
+            border: 1px solid #FCDC13;
+            background-color: transparent;
             color: #FCDC13;
             border-radius: 4px;
         }
 
-        nav.filter select option:hover {
-            background-color: #000000;
+        .form-container input[type="submit"] {
+            background-color: #FCDC13;
+            color: #121212;
+            border: none;
+            cursor: pointer;
         }
 
-        .myclasses {
-            color: #FCDC13;
-            width: 50%;
-            margin: 80px auto;
-            z-index: 1000;
-            justify-content: center;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            text-align: center;
-        }
-
-        .video-container {
-            position: relative;
-            padding-bottom: 56.25%;
-            height: 0;
-            overflow: hidden;
-            max-width: 100%;
-            background-color: #fff;
-            margin: 20px auto;
-
-        }
-
-        .video-container iframe {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            border: 0;
-        }
-
-        .deliveringsummary {
-            background: #121212;
-        }
-
-        .deliveringsummary h1 {
-            text-align: center;
-            color: #FCDC13;
-
-        }
-
-        .deliveringsummary p {
-            text-align: center;
-            color: #FCDC13;
+        .form-container input[type="submit"]:hover {
+            background-color: #FFEEAD;
         }
     </style>
 </head>
 
 <body>
+    <header>
+        <a href="{{ route('index') }}"><img src="\storage\logoievv.png" alt="" class="logo"></a>
+        <nav class="navegation">
+            <ul>
+                <li><a href="{{ route('perfil') }}">Perfil</a></li>
+                <li><a href="{{ route('index') }}">Sair</a></li>
+            </ul>
+        </nav>
+    </header>
+
     <section>
-        <header>
-            <a href="{{ route('index') }}"><img src="\storage\logoievv.png" alt="" class="logo"></a>
-            <nav class="navegation">
-                <ul>
-                    <li><a href="{{ route('perfil') }}">Perfil</a></li>
-                    <li><a href="{{ route('index') }}">Sair</a></li>
-                </ul>
-            </nav>
-        </header>
+        <div class="myclasses">
+            <iframe width="500" height="350" src="https://www.youtube.com/embed/TVTjeo97b7I" frameborder="0" allowfullscreen></iframe>
+            <div class="text-content">
+                <h1>DÍZIMOS E OFERTAS</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo velit dolores necessitatibus odio vitae corrupti fugiat, ipsum sit nihil accusantium possimus tempore magni saepe laborum, id provident minima? Eveniet, vero!</p>
+            </div>
+        </div>
 
         <nav class="filter">
             <select id="categoria">
@@ -160,23 +225,29 @@
                 <option value="Envolva-se">Envolva-se</option>
             </select>
         </nav>
+    </section>
+    <<section>
+        <div class="deliveringsummary">
+            <h1>FINALIZOU?</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laboriosam. Perspiciatis qui quam ducimus modi soluta, velit, aperiam aliquid dolore libero corporis ad aspernatur voluptatum. Dicta dolore eius deserunt alias <br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nesciunt vitae vero commodi quidem veritatis ea dolorem modi facilis libero, beatae sequi veniam obcaecati quaerat unde ut tempore quae corrupti. <br> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod modi provident repellat, assumenda, voluptatem fuga natus et error consequuntur enim facere reiciendis blanditiis sequi adipisci ducimus quis nam nisi quasi!</p>
+        </div>
 
-        <div class="myclasses">
-            <iframe width="500" height="350" src="https://www.youtube.com/embed/TVTjeo97b7I" frameborder="0" allowfullscreen></iframe>
-            <br>
-            <h1>DÍZIMOS E OFERTAS</h1>
-            <br>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo velit dolores necessitatibus odio vitae corrupti fugiat, ipsum sit nihil accusantium possimus tempore magni saepe laborum, id provident minima? Eveniet, vero!</p>
+        <div class="form-container">
+            <h2>Tire suas dúvidas sobre a aula</h2>
+            <form action="#">
+                <label for="name">Nome:</label>
+                <input type="text" id="name" name="name" required>
+
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+
+                <label for="message">Mensagem:</label>
+                <textarea id="message" name="message" rows="4" required></textarea>
+
+                <input type="submit" value="Enviar">
+            </form>
         </div>
     </section>
-    <nav class="deliveringsummary">
-        <h1>FINALIZOU?</h1>
-        <br>
-        <br>
-        <P>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab, laboriosam. Perspiciatis qui quam ducimus modi soluta, <br>velit, aperiam aliquid dolore libero corporis ad aspernatur voluptatum. Dicta dolore eius deserunt alias.</P>
-        <input type="file">
-    </nav>
-
 </body>
 
 </html>
